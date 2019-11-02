1a6fafb7dcff8244ade260bc2a3bf47f9f08264d
Manuel Mihailov <blanksenpai123@gmail.com>
2019-11-02 14:14:20 +0200

    Started making database cocktail prefill method

--- CocktailMagicianVM/ServiceLayer/CocktailService.cs
+++ CocktailMagicianVM/ServiceLayer/CocktailService.cs
@@ -1,10 +1,40 @@
-﻿using System;
+﻿using Data;
+using Data.Models;
+using Newtonsoft.Json;
+using ServiceLayer.Contracts;
+using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace ServiceLayer
 {
-    public class CocktailService
+    public class CocktailService : ICocktailService
     {
+        private readonly CocktailDatabaseContext dbContext;
+        private readonly IAccountService aService;
+
+        public CocktailService(CocktailDatabaseContext dbContext, IAccountService aService)
+        {
+            this.dbContext = dbContext;
+            this.aService = aService;
+        }
+        public async Task DatabaseCocktailFill()
+        {
+            string drinksToRead = File.ReadAllText(@"../../../../Data/SolutionPreload/Cocktails.json");
+            List<CocktailJason> listOfDrinks = JsonConvert.DeserializeObject<List<CocktailJson>>(drinksToRead);
+
+            if (dbContext.Cocktail.Count() == 0)
+            {
+                foreach (var item in listOfDrinks)
+                {
+
+                }
+
+            }
+        }
+
     }
 }
