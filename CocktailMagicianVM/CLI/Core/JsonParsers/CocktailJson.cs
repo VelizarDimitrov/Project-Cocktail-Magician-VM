7bc58e8c226a48fdb151b127ddc90b319591da92
Manuel Mihailov <blanksenpai123@gmail.com>
2019-11-02 13:47:39 +0200

    Installed Json NuGet in the correct place now and filled the Json to Entity helping class with the required properties.

--- CocktailMagicianVM/CLI/Core/JsonParsers/CocktailJson.cs
+++ CocktailMagicianVM/CLI/Core/JsonParsers/CocktailJson.cs
@@ -7,6 +7,8 @@ namespace CLI.Core.JasonParsers
 {
     public class CocktailJson : ICocktailJson
     {
-        
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string[] Ingredient { get; set; }
     }
 }
