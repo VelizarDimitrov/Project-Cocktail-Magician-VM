9684ad197e9716062e0dccdd5901c18cea99c2ff
Velizar Dimitrov <velizar.dimitrov.bg@gmail.com>
2019-11-02 14:10:42 +0200

    Fixed parsing classes for users and bars.

--- CocktailMagicianVM/CLI/Core/JsonParsers/Contracts/IBarJson.cs
+++ CocktailMagicianVM/CLI/Core/JsonParsers/Contracts/IBarJson.cs
@@ -6,5 +6,11 @@ namespace CLI.Core.JasonParsers.Contracts
 {
     public interface IBarJson
     {
+        string Name { get; set; }
+        string Address { get; set; }
+        string Description { get; set; }
+        string Country { get; set; }
+        string City { get; set; }
+        string BarCoverPath { get; set; }
     }
 }
