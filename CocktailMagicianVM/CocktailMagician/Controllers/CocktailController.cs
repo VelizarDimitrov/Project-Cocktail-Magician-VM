7410b1b59972b8eb8b83c98c7e62862a3d29a324
Velizar Dimitrov <velizar.dimitrov.bg@gmail.com>
2019-11-14 16:09:07 +0200

    Started creating Add Cocktail .

---
+++ CocktailMagicianVM/CocktailMagician/Controllers/CocktailController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CocktailMagician.Controllers
+{
+    public class CocktailController : Controller
+    {
+        public IActionResult AddCocktail()
+        {
+
+            return View("AddCocktail");
+        }
+    }
+}
---
+++ CocktailMagicianVM/CocktailMagician/Models/AddCocktailViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CocktailMagician.Models
+{
+    public class AddCocktailViewModel
+    {
+        public AddCocktailViewModel()
+        {
+            MainIngredients = new List<string>();
+            Ingredients = new List<string>();
+        }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public IList<string> MainIngredients { get; set; }
+        public IList<string> Ingredients { get; set; }
+    }
+}
