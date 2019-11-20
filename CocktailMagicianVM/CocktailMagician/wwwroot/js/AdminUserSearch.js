914350ac70f74e1ac904d9422e7ec4524c4eee94
Velizar Dimitrov <velizar.dimitrov.bg@gmail.com>
2019-11-20 22:27:56 +0200

    Fixed some minor issues.

---
+++ CocktailMagicianVM/CocktailMagician/wwwroot/js/AdminUserSearch.js
@@ -0,0 +1,59 @@
+﻿function unFreezeUser(userId) {
+    $.ajax({
+        url: '/administration/admin/unfreezeuser',
+        type: "POST",
+        data: { userId: userId },
+        success: function () {
+            changePage(0);
+        }
+    });
+}
+function freezeUser(userId) {
+    $.ajax({
+        url: '/administration/admin/freezeuser',
+        type: "POST",
+        data: { userId: userId },
+        success: function () {
+            changePage(0);
+        }
+    });
+}
+function promoteUser(userId) {
+    $.ajax({
+        url: '/administration/admin/promoteuser',
+        type: "POST",
+        data: { userId: userId },
+        success: function () {
+            changePage(0);
+        }
+    });
+}
+function demoteUser(userId) {
+    $.ajax({
+        url: '/administration/admin/demoteuser',
+        type: "POST",
+        data: { userId: userId },
+        success: function () {
+            changePage(0);
+        }
+    });
+}
+
+const transparency = function () {
+    $('body').attr('class', 'not-transparent-header');
+}
+window.onload = transparency;
+
+function changePage(number) {
+    let keyword = $('#current-keyword').val();
+    let page = parseInt($('#current-page').val()) + number;
+    let pageSize = 15;
+    $('#search-results').load('/administration/admin/usersearchresults', { keyword: keyword, page: page, pageSize: pageSize });
+}
+function searchEventHandler() {
+    let keyword = $('#keyword').val();
+    let page = 1;
+    let pageSize = 15;
+    $('#search-results').load('/administration/admin/usersearchresults', { keyword: keyword, page: page, pageSize: pageSize });
+}
+searchEventHandler();
