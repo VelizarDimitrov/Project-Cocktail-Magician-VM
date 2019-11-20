f46ae2e9259fc41224627593aedcaddf37163ce2
Velizar Dimitrov <velizar.dimitrov.bg@gmail.com>
2019-11-20 19:19:10 +0200

    Started creating the Admin area .

---
+++ CocktailMagicianVM/CocktailMagician/Areas/Administration/Controllers/AdminController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CocktailMagician.Areas.Administration.Models;
+using CocktailMagician.Models;
+using Data.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using ServiceLayer.Contracts;
+
+namespace CocktailMagician.Areas.Administration.Controllers
+{
+    [Area("Administration")]
+    [Authorize(Roles = "Admin")]
+    public class AdminController : Controller
+    {
+        private readonly IAccountService aService;
+
+        public AdminController(IAccountService aService)
+        {
+            this.aService = aService;
+        }
+
+        public IActionResult ManageUsers()
+        {
+            return View("Users");
+        }
+    }
+    public async Task<IActionResult> UserSearchResults(string keyword, string page, string pageSize)
+    {
+        Tuple<IList<User>, bool> users;
+        var model = new UserSearchViewModel()
+        {
+            Keyword = keyword == null ? "" : keyword,
+            Page = int.Parse(page)
+        };
+        users = await aService.FindUsersForAdminAsync(model.Keyword, model.Page, int.Parse(pageSize));
+
+        foreach (var user in users.Item1)
+        {
+            model.Users.Add(new UserViewModel(user));
+        }
+        model.LastPage = users.Item2;
+        return PartialView("_AdminUsersResultView", model);
+    }
+}
---
+++ CocktailMagicianVM/CocktailMagician/Areas/Administration/Models/UserSearchViewModel.cs
@@ -0,0 +1,24 @@
+﻿using CocktailMagician.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CocktailMagician.Areas.Administration.Models
+{
+    public class UserSearchViewModel
+    {
+        public UserSearchViewModel()
+        {
+            Users = new List<UserViewModel>();
+        }
+        public bool Searched { get; set; }
+        public string SelectedCriteria { get; set; }
+        public string SelectedOrderBy { get; set; }
+        public int Page { get; set; }
+        public string Keyword { get; set; }
+        public bool LastPage { get; set; }
+        public List<UserViewModel> Users { get; set; }
+    }
+}
+}
